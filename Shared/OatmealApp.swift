524451ce0164eec1dd222dacaeac10a8fa09487f
Brianna Doubt <bri@briannadoubt.com>
2021-10-31 13:56:44 -0700

    Initial Commit

---
+++ Shared/OatmealApp.swift
@@ -0,0 +1,17 @@
+//
+//  OatmealApp.swift
+//  Shared
+//
+//  Created by Bri on 10/31/21.
+//
+
+import SwiftUI
+
+@main
+struct OatmealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
